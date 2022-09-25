ebd6afe8cf5517aa74b8660e50b0eace136ea169
Trabatu <kluge.lukasz@gmail.com>
2022-09-25 13:44:09 +0200

    final stage of the game

--- script.js
+++ script.js
@@ -12,34 +12,54 @@ const WINNING_COMBINATIONS = [
 ]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelectorAll('[data-winning-message-text]')
 let circleTurn
+
 startGame()
 
-function startGame(){
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
     circleTurn = false
     cellElements.forEach(cell => {
-        cell.addEventListener('click', handleClick, {once: true})
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.addEventListener('click', handleClick, { once: true })
     })
     setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
 }
-cellElements.forEach(cell =>  {
-    cell.addEventListener('click', handleClick, { once: true})
-})
 
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
     if (checkWin(currentClass)) {
-        console.log('winner')
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
     }
-    // Check for win //
-    // Check for draw //
-    // Switch turns //
-    swapTurns()
-    setBoardHoverClass()
 }
 
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!'
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
+}
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
